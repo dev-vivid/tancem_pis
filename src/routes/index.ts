18149becf575eca86cc65faeab3a7e90772b904f
manojKumarbalu <manojkumarbalu555@gmail.com>
2025-08-11 11:22:55 +0530

    fix: Update default ID generation to use uuid() for consistency across models

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,11 @@ import { baseAuth } from "@middleware/auth";
 
 import analysisRouter from "@module/v1/master/routes/analysis.routes";
 import problemCodeRouter from "@module/v1/master/routes/problemCode.routes";
+import problemRouter from "@module/v1/master/routes/problem.routes";
 
 const routes = Router();
 
 routes.use(``, [baseAuth], analysisRouter);
 routes.use(``, [baseAuth], problemCodeRouter);
-
+routes.use(/problem, [baseAuth], problemRouter);
 export { routes as apiV1Routes };
