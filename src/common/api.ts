a947a8b49e59e39d33a1e59416a0f2d4729738e5
ABISHAKE <abishake887@gmail.com>
2025-09-02 15:54:13 +0530

    feat: add functions to retrieve department, office, and equipment subgroup names from user management API

--- src/common/api.ts
+++ src/common/api.ts
@@ -3,6 +3,7 @@ import {
 	SESSION_VALIDATION_URL,
 	INVENTORY_URL,
 	ASSET_URL,
+	USER_MANAGEMENT_URL
 } from "@config/index";
 
 // export const getMaterialName = async (
@@ -99,6 +100,112 @@ export const getEquipmentName = async (
 	} catch (error) {
 		console.error(
 			`Error fetching equipment name for ID ${equipmentId}:`,
+			error
+		);
+		return null;
+	}
+};
+
+export const getDepartmentName = async (
+	departmentId: string,
+	accessToken: string
+) => {
+	const token = `Bearer ${accessToken}`;
+	try {
+		const response = await fetch(
+			`${USER_MANAGEMENT_URL}/section/${departmentId}`,
+			{
+				headers: new Headers({
+					"content-type": "application/json",
+					authorization: token,
+				}),
+				method: "GET",
+			}
+		);
+
+		const data = await response.json();
+		// console.log("API Raw Response:", data);
+
+		// ✅ Check response structure
+		if (data?.success && data?.data) {
+			return data.data; 
+		}
+
+		return null;
+	} catch (error) {
+		console.error(
+			`Error fetching equipment name for ID ${departmentId}:`,
+			error
+		);
+		return null;
+	}
+};
+
+export const getOfficeName = async (
+	officeId: string,
+	accessToken: string
+) => {
+	const token = `Bearer ${accessToken}`;
+	try {
+		const response = await fetch(
+			`${USER_MANAGEMENT_URL}/office?table=office`,
+			
+			{
+				headers: new Headers({
+					"content-type": "application/json",
+					authorization: token,
+				}),
+				method: "GET",
+			}
+		);
+
+		const data = await response.json();
+		// console.log("API Raw Response:", data);
+
+		// ✅ Check response structure
+		if (data?.success && data?.data?.data) {
+			return data.data.data; 
+		}
+
+		return null;
+	} catch (error) {
+		console.error(
+			`Error fetching equipment name for ID ${officeId}:`,
+			error
+		);
+		return null;
+	}
+};
+
+export const getEquipmentSubGroupName = async (
+	equipmentSubGroupId: string,
+	accessToken: string
+) => {
+	const token = `Bearer ${accessToken}`;
+	try {
+		const response = await fetch(
+			`${ASSET_URL}/equipmentSubGroupView/${equipmentSubGroupId}`,
+			{
+				headers: new Headers({
+					"content-type": "application/json",
+					authorization: token,
+				}),
+				method: "GET",
+			}
+		);
+
+		const data = await response.json();
+		// console.log("API Raw Response:", data);
+
+		// ✅ Check response structure
+		if (data?.success && data?.data?.list) {
+			return data.data.list;
+		}
+
+		return null;
+	} catch (error) {
+		console.error(
+			`Error fetching equipment name for ID ${equipmentSubGroupId}:`,
 			error
 		);
 		return null;
